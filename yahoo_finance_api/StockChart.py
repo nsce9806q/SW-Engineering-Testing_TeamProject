6265c2cd8c3e8a9c85ab09f504d1022c66fbfcdf
jhyep <pjh1115_@naver.com>
2022-05-31 18:42:21 +0900

    fix: 테스트

--- yahoo_finance_api/StockChart.py
+++ yahoo_finance_api/StockChart.py
@@ -9,6 +9,7 @@ chart = pdr.get_data_yahoo(code, start = '2022-05-01')
 print(chart)
 #print(chart['Close']) - 종가만 출력
 
+# 수정
 
 plt.plot(chart.index, chart.Close, 'b') #날짜 정보, 종가 정보, 파란색
 plt.show()
