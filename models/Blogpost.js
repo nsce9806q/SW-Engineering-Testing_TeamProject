4b1314a30d7390466f5921513b42a1c6a28da69e
Gyu-won <54783290+Gyu-won@users.noreply.github.com>
2022-06-08 15:45:43 +0900

    Update Blogpost.js
    주석 추가 및 update 날짜 기능 구현

--- models/Blogpost.js
+++ models/Blogpost.js
@@ -1,14 +1,19 @@
+// 데이터베이스 스키마
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
-    title : String,
-    body : String,
+    title : {
+        type : String,
+        required : true
+    },
+    body : {
+        type : String,
+        required : true
+    },
     username : String,
-    datePosted : {
-        type : Date,
-        default : new Date()
-    }
+    }, {
+        timestamps: true,
 });
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
