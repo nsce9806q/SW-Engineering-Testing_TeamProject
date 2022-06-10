15a20ac2a8c7f09267f6f1aa4909bb449175da50
nsce9806q <nsce9806q@gmail.com>
2022-06-10 08:34:37 +0000

    디렉토리 구조 변경

--- backend/src/api/auth/index.js
+++ backend/src/api/auth/index.js
@@ -4,13 +4,12 @@ const validator = require('../../middleware/validator');
 const login = require('../../service/auth/login');
 const register = require('../../service/auth/register');
 const checkLogin = require('../../middleware/passport/jwtAuthentication');
-const passport = require('passport');
 
 router
     .post('/login', validator.auth.login, login)
     .post('/register', validator.auth.register, register)
-    .get('/test', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-        res.send("good!");
+    .get('login_test', checkLogin, (req, res, next) => {
+        res.send("로그인 확인");
     });
 
 module.exports = router;
