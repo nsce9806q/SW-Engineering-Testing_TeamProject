c7e353d6807fe25655c81f0e3b5473abaeda1426
nsce9806q <nsce9806q@gmail.com>
2022-06-10 09:11:39 +0000

    fix(controller): CORS 허용

--- backend/src/loaders/express.js
+++ backend/src/loaders/express.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const api = require('../api');
+const cors = require('cors');
 
 module.exports = (app) => {
     app.use(express.json());
+    app.use(cors())
     app.use('/', api);
 
     app.listen(3000, () => {
